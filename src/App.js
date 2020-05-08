9289826ed48f9b2d268999fdd621c2b8ef3d3b18
vishnu777aero <vishnu777aero@gmail.com>
2020-05-08 19:43:19 +0530

    Remove leftover codes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.scss';
-import { CustomBoxCursor } from './Components/CustomBoxCursor';
 
 const App = () => {
   return (
-      <CustomBoxCursor />
+      <div>We are tensionless springs :)</div>
   );
 }
 
