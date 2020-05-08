f01915cc7735248506d43d632d872edfcb920d40
vishnu777aero <vishnu777aero@gmail.com>
2020-05-08 22:45:26 +0530

    Add single spring oscillation

--- src/Springs/OscillatingBar/index.js
+++ src/Springs/OscillatingBar/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 
 export const OscillatingBar = () => {
+    const radialSpring = {
+        from: {
+            transform: `rotate(-22.5deg)`
+        },
+        to: {
+            transform: `rotate(0deg)`
+        },
+        config: {
+            tension  : 40,
+            mass     : 1,
+            friction : 0
+        }
+    };
+
+    const radialStyle = useSpring(radialSpring);
+
     return (
-        <span className='oscillating-bar' />
+        <animated.span 
+            className='oscillating-bar'
+            style={radialStyle} />
     );
 }
