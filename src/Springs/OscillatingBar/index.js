7e2f2d4511032f7d8281cbc7860d332cbc6ee7fc
vishnu777aero <vishnu777aero@gmail.com>
2020-05-09 00:56:24 +0530

    Add dual spring oscillation

--- src/Springs/OscillatingBar/index.js
+++ src/Springs/OscillatingBar/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { useSpring, animated } from 'react-spring';
+import { useSprings, animated } from 'react-spring';
 
 export const OscillatingBar = () => {
+    //useSprings is good for static lists of items,(with the help of .map() and interpolate())
+    //and not for a single element with multiple springs.
+    //This example works but is a bad one w.r.t syntax.
+
     const radialSpring = {
         from: {
-            transform: `rotate(-22.5deg)`
+            transform: `rotate(-22deg)`
         },
         to: {
             transform: `rotate(0deg)`
@@ -16,11 +20,30 @@ export const OscillatingBar = () => {
         }
     };
 
-    const radialStyle = useSpring(radialSpring);
+    const horizontalSpring = {
+        from: {
+            width: `10vw`
+        },
+        to: {
+            width: `20vw`
+        },
+        config: {
+            tension  : 10,
+            mass     : 1,
+            friction : 0
+        }
+    };
+
+    const springs = [
+        radialSpring,
+        horizontalSpring
+    ];
+
+    const style = useSprings(2, i => springs[i]);
 
     return (
         <animated.span 
             className='oscillating-bar'
-            style={radialStyle} />
+            style={{ ...style[0][0], ...style[0][1]}}/>
     );
 }
